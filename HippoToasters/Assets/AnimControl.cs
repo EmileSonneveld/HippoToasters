521bc54fc00229c4f9c205f2a01b950f3bf1f543
Qendrim Tuzi <qendrimtuzi@hotmail.Com>
2018-10-06 14:11:13 +0200

    bonhomme

---
+++ HippoToasters/Assets/AnimControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimControl : MonoBehaviour {
+    public float horizontalMove = 0f;
+    public Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ HippoToasters/Assets/qendrim/scripts/fallingsword.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingsword : MonoBehaviour {
+    public GameObject player1;
+    public GameObject arrow;
+	// Use this for initialization
+	void Start () {
+        player1 = GameObject.FindGameObjectWithTag("Player");
+        arrow.GetComponent<Rigidbody2D>().simulated = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player") 
+        {
+            arrow.GetComponent<Rigidbody2D>().simulated = true;
+        }
+    }
+}
