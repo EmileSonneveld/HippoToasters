818d1fb8addf55ebe269e7190fcd9805facfdbcb
Qendrim Tuzi <qendrimtuzi@hotmail.Com>
2018-10-06 19:15:46 +0200

    test

--- HippoToasters/Assets/animationIOControl.cs
+++ HippoToasters/Assets/animationIOControl.cs
@@ -7,20 +7,10 @@ public class animationIOControl : MonoBehaviour {
     
 	// Use this for initialization
 	void Start () {
-        animIo.SetBool("jumpIo", false) ;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space)){
-            animIo.SetBool("jumpIo", true);
-        }
-        if (Input.GetKeyUp(KeyCode.Space) )
-        {
-            animIo.SetBool("jumpIo", false);
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.LeftShift));
     }
   
 }
